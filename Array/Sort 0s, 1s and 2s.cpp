517aa271ea0ed62517f9b887df4d9eac5fdfb7a5
Satyam Singh <75965342+satyamsingh-stack@users.noreply.github.com>
2022-03-02 17:45:05 +0530

    Create Sort 0s, 1s and 2s.cpp

---
+++ Array/Sort 0s, 1s and 2s.cpp
@@ -0,0 +1,20 @@
+void sort012(int arr[], int n)
+    {
+        int c0=0,c1=0,c2=0;
+        for(int i=0;i<n;i++){
+            if(arr[i]==0)
+                c0++;
+            else if(arr[i]==1)
+                c1++;
+            else
+                c2++;
+        }
+        for(int i=0;i<n;i++){
+            if(i<c0)
+                arr[i]=0;
+            else if(i>=c0 && i<c1+c0)
+                arr[i]=1;
+            else
+                arr[i]=2;
+        }
+    }
